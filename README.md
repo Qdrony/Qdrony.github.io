7496d981b264cd7e5d983dd6ed1a42139c5c4b32
Kudron Máté <h160569@stud.u-szeged.hu>
2025-11-01 18:10:27 +0100

    domain update, redirect to new home page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kudibusz
-A KudiBusz Hivatalos Weboldal
--- js/upisdown.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".accordion-button").forEach(button => {
-        button.addEventListener("click", function () {
-            this.classList.toggle("active");
-        });
-    });
-});
